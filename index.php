0576753c04b58f524e9c13c71e7233cc2065d578
Dimitris Giakoumis <d.giakoumis@7linternational.com>
2023-04-11 12:56:01 +0300

    Update index.php
    change index for dev

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>This is node 3</p>'; ?> 
+ <?php echo '<p>This is the dev code, it does silly things</p>'; ?> 
  </body>
 </html>
